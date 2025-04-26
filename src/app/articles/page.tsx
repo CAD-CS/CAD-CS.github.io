42cb05482078cf56456e91380ff94e5e503ff34b
CAD-CS <parsa.hara117@gmail.com>
2025-04-26 15:19:12 -0700

    Added List component

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,11 +1,22 @@
 import { Metadata } from "next";
+import PageHeader from "../_components/PageHeader";
+import ContentBox from "../_components/ContentBox";
+import List from "../_components/List";
+import ListLink from "../_components/ListLink";
 
 export const metadata: Metadata = {
-  title: 'Projects',
+  title: 'Articles',
 };
 
 export default function Articles() {
   return (
-    <p>Articles</p>
+    <>
+      <PageHeader text="Articles" content="A collection of technical articles I have written" />
+      <ContentBox>
+        <List>
+          <ListLink basePath="articles" targetFolder="alternative-designs" text="Monads, Forks, And Hooks: Alternative Design Patterns In Non-Object Oriented Languages" />
+        </List>
+      </ContentBox>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import ContentBox from "./_components/ContentBox";
 import PageHeader from "./_components/PageHeader";
+import ContentHeader from "./_components/ContentHeader";
+import ContentParagraph from "./_components/ContentParagraph";
 
 export const metadata: Metadata = {
   title: 'Home'
@@ -11,19 +13,19 @@ export default function Home() {
     <>
       <PageHeader text="Home" content="A little about myself" />
       <ContentBox>
-        <h1 className="text-3xl font-bold underline mb-6">Who am I?</h1>
-        <p className="text-lg leading-relaxed mb-4">
+        <ContentHeader>Who am I?</ContentHeader>
+        <ContentParagraph>
           I am a software developer and IT support analyst passionate about solving challenges and delivering value. A quick learner and team player, I bring technical expertise and adaptability to any environment.
-        </p>
-        <p className="text-lg leading-relaxed mb-4">
+        </ContentParagraph>
+        <ContentParagraph>
           My experience spans multiple programming languages, including JavaScript, TypeScript, Python, Java, and C++. I am proficient in frameworks like React and Next.js for web development, Jest and JUnit for testing, and D3 for data visualization. Additionally, I have extensive knowledge of SQL (PostgreSQL), database management tools like pgAdmin, and DevOps tools such as Docker and GitHub Actions. My projects range from building websites and text editors to developing data analytics and visualization tools, showcasing my ability to master new technologies quickly.
-        </p> 
-        <p className="text-lg leading-relaxed mb-4">
-          In IT support, I provide confidential troubleshooting and consultations for university faculty, ensuring privacy and effective technical communication. I have a deep understanding of business needs and privacy considerations, enabling me to resolve software issues efficiently, translate technical concepts for non-technical audiences, write clear documentation, and handle sensitive information with discretion. My hands-on experience includes industry-standard tools like ServiceNow for ticketing, Confluence for documentation, and Microsoft Power Automate.
-        </p>
-        <p className="text-lg leading-relaxed">
+        </ContentParagraph> 
+        <ContentParagraph>
+          As an IT expert, I provide confidential troubleshooting and consultations for university faculty, ensuring privacy and effective technical communication in addition to having a deep understanding of business needs and privacy considerations. I have experience resolving software issues efficiently, translating technical concepts for non-technical audiences, writing clear documentation, and handling sensitive information with discretion. My hands-on experience includes industry-standard tools like ServiceNow for ticketing, Confluence for documentation, and Microsoft Power Automate.
+        </ContentParagraph>
+        <ContentParagraph>
           Beyond technical skills, my greatest strengths lie in collaboration, client communication, conflict resolution, and mentoring junior hires. I thrive in team environments, ensuring successful project outcomes and fostering positive workplace relationships.
-        </p>
+        </ContentParagraph>
       </ContentBox>
     </>
   );
