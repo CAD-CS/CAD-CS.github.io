2dda15428ee9b8b9eecda1a9257f431790c7aa9f
CAD-CS <parsa.hara117@gmail.com>
2025-12-20 21:37:55 -0800

    Added text to pages

--- components/home-page.js
+++ components/home-page.js
@@ -3,17 +3,25 @@ template.innerHTML = `
 <main-content>
 	<ascii-art></ascii-art>
 	<h1>Welcome!</h1>
-	<p>This is the home page.</p>
+	<pre></pre>
 </main-content>
 `;
 
-class HomePage extends HTMLElement 
-{
-    constructor() 
-    {
+class HomePage extends HTMLElement {
+    constructor() {
         super();
         this.root = this.attachShadow({ mode: 'open' });
         this.root.append(template.content.cloneNode(true));
+    }
+
+    async connectedCallback() {
+        const text = await this.getText();
+        this.root.querySelector('pre').textContent = text;
+    }
+
+    async getText() {
+        const art = await fetch('./text/home.txt');
+        return await art.text();
     }
 }
 
--- components/site-info-page.js
+++ components/site-info-page.js
@@ -1,17 +1,25 @@
 const template = document.createElement('template');
 template.innerHTML = `
 <main-content>
-    Site info
+    <pre></pre>
 </main-content>
 `;
 
-class SiteInfoPage extends HTMLElement 
-{
-    constructor() 
-    {
+class SiteInfoPage extends HTMLElement {
+    constructor() {
         super();
         this.root = this.attachShadow({ mode: 'open' });
         this.root.append(template.content.cloneNode(true));
+    }
+
+    async connectedCallback() {
+        const text = await this.getText();
+        this.root.querySelector('pre').textContent = text;
+    }
+
+    async getText() {
+        const art = await fetch('./text/site-info.txt');
+        return await art.text();
     }
 }
 
